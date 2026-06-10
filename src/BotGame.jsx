4dd2f57b46d8fccdbcc362570d28b765005e1da5
misiprrc <misip@Academic.rrc.ca>
2026-06-09 23:16:08 -0500

    Added forfeit button in bot game

--- src/BotGame.jsx
+++ src/BotGame.jsx
@@ -1,8 +1,13 @@
 function BotGame({ setScreen }) {
   return (
-    <div>
-      <p>Hello BotGame</p>
-      <button onClick={() => setScreen("menu")}>Return</button>
+    <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex flex-col gap-10 items-center justify-center">
+      <p>Rock Paper Scissors Lizard Spock</p>
+      <button
+        onClick={() => setScreen("menu")}
+        className="bg-red-400 p-4 rounded-2xl text-2xl"
+      >
+        Forfeit
+      </button>
     </div>
   );
 }
