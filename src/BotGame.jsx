a67b601f69dbd4a1625f894ecad4e40ed1fcc304
marcisip35 <marckevinisip@hotmail.com>
2026-06-09 23:20:50 -0500

    Testing git author

--- src/BotGame.jsx
+++ src/BotGame.jsx
@@ -1,10 +1,10 @@
 function BotGame({ setScreen }) {
   return (
-    <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex flex-col gap-10 items-center justify-center">
+    <div className="botGame top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed flex flex-col gap-10 items-center justify-center">
       <p>Rock Paper Scissors Lizard Spock</p>
       <button
         onClick={() => setScreen("menu")}
-        className="bg-red-400 p-4 rounded-2xl text-2xl"
+        className="bg-red-400 p-4 rounded-2xl text-2xl border-2 border-red-400"
       >
         Forfeit
       </button>
